82a9a680afb50065bda8f36e8e6a981cfe4ddfdd
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-08-07 20:50:52 +0500

    tophaeder start

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../core/layout/sidebar/sidebar.component';
+import { TopHeaderComponent } from '../../core/layout/top-header/top-header.component';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, TopHeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
