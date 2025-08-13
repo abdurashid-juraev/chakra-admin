122450234b92ae5790ea2960d35049c28ede3994
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2025-08-13 09:53:58 +0500

    billing in process

--- src/app/features/billing/billing.component.ts
+++ src/app/features/billing/billing.component.ts
@@ -6,4 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './billing.component.html',
   styleUrl: './billing.component.css',
 })
-export default class BillingComponent {}
+export default class BillingComponent {
+  public payCards = [
+    {
+      icon: '/images/svg/wallet.svg',
+      title: 'Salary',
+      subtitle: 'Belong Interactive',
+      amount: '+$2000',
+      iconBg: 'bg-primary',
+    },
+    {
+      icon: '/images/svg/paypal.svg',
+      title: 'Paypal',
+      subtitle: 'Freelance Payment',
+      amount: '$455.00',
+      iconBg: 'bg-primary',
+    },
+  ];
+}
