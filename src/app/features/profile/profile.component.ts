b1ba57b77dcf4d3d08d5e0e68be523eda04d49b0
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2026-04-20 14:50:32 +0500

    table primeng style

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,14 +1,28 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, signal } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IStudents } from './common/models';
+import { NgTemplateOutlet } from '@angular/common';
+import { TableComponent } from '../../shared/components/table/table.component';
 
 @Component({
   selector: 'app-profile',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, NgTemplateOutlet, TableComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class ProfileComponent {
-  public students = signal<IStudents[]>([]);
+  public students = signal<IStudents[]>([
+    { id: 1, fullName: 'Anvarov Dilshod', status: 'success', grade: 85 },
+    { id: 2, fullName: 'Karimova Malika', status: 'success', grade: 92 },
+    { id: 3, fullName: 'Tursunov Jamshid', status: 'failed', grade: 45 },
+    { id: 4, fullName: 'Sultonova Zilola', status: 'process', grade: 78 },
+    { id: 5, fullName: 'Abduvaliyev Otabek', status: 'success', grade: 88 },
+    { id: 6, fullName: 'Zokirova Madina', status: 'failed', grade: 30 },
+    { id: 7, fullName: 'Rustamov Sardor', status: 'success', grade: 95 },
+    { id: 8, fullName: 'Ismoilova Guli', status: 'process', grade: 72 },
+    { id: 9, fullName: 'Hamidov Alisher', status: 'process', grade: 50 },
+    { id: 10, fullName: 'Yusupova Nigora', status: 'success', grade: 81 },
+  ]);
+  public show = signal<boolean>(true);
 }
--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, input, TemplateRef } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-table',
-  imports: [],
+  standalone: true,
+  imports: [NgTemplateOutlet],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.css'
+  styleUrl: './table.component.css',
 })
-export class TableComponent {
-
+export class TableComponent<T> {
+  public data = input<T[]>([]);
+  public theadTemplate = input<TemplateRef<{ $implicit: T; index: number }> | null>(null);
+  public rowTemplate = input<TemplateRef<{ $implicit: T; index: number }> | null>(null);
 }
