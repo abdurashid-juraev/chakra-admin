b370094d56b93d746dc6d754b5b5c898be7a6ca3
Abdurashid <80646202+abdurashid-juraev@users.noreply.github.com>
2026-06-04 18:45:30 +0500

    feat: ONEID login by modal

--- src/app/features/profile/profile.component.ts
+++ src/app/features/profile/profile.component.ts
@@ -1,45 +1,54 @@
-import { filter } from 'rxjs';
-import { ChangeDetectionStrategy, Component, computed, input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, OnInit, signal } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Button } from 'primeng/button';
 import { InputIcon } from 'primeng/inputicon';
 import { IconField } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
 import { User } from './common/models';
+import { TableModule } from 'primeng/table';
 @Component({
   selector: 'app-profile',
   imports: [
     FormsModule,
     ReactiveFormsModule,
-    Button,
     InputIcon,
     IconField,
     InputIcon,
     InputTextModule,
+    Button,
+    TableModule,
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ProfileComponent {
-  public todo = new FormControl('');
+export default class ProfileComponent implements OnInit {
   public users = signal<User[] | []>([]);
-  protected currentId = signal<number | null>(null);
-  protected isEditing = signal<boolean>(false);
+  protected name = new FormControl('');
 
-  ngOnInit() {
-    this.loadData();
+  ngOnInit(): void {}
+
+  private saveData(): void {
+    localStorage.setItem('users', JSON.stringify(this.users()));
+  }
+
+  private loadData(): void {
+    const users = localStorage.getItem('users');
+    if (users) {
+      this.users.update(loadUsers => [...loadUsers, JSON.parse(users)]);
+      console.log(users);
+    }
   }
 
   private create() {
-    const name = this.todo.value?.trim();
-    if (!name) return alert('Input is required..!');
+    const name = this.name.value?.trim();
+
+    if (!name) return alert('Name is required..!');
 
     const id = computed((): number => {
-      const userList: User[] = this.users();
-
+      const userList = this.users();
       if (userList.length > 0) {
-        return Math.max(...userList.map(user => user.id)) + 1;
+        return Math.max(...userList.map((user: User) => user.id)) + 1;
       }
       return 1;
     });
@@ -47,62 +56,15 @@ export default class ProfileComponent {
     const newUser: User = {
       id: id(),
       name: name,
-      email: `${name}@gmail.com`,
+      email: `${name}@mail.com`,
     };
 
     this.users.update(currentUser => [...currentUser, newUser]);
-    this.todo.reset();
-  }
-  private saveData(): void {
-    localStorage.setItem('users', JSON.stringify(this.users()));
-  }
-  private loadData(): void {
-    const savedUsers = localStorage.getItem('users');
-    if (savedUsers) {
-      try {
-        const parseUsers = JSON.parse(savedUsers);
-        this.users.set(parseUsers);
-      } catch (error) {
-        console.error('Xatolik..!', error);
-      }
-    }
-  }
-
-  protected deleteBtn(id: number) {
-    this.users.update(currentUser => currentUser.filter(user => user.id !== id));
-  }
-  protected editBnt(id: number) {
-    const user = this.users().find(user => user.id === id);
-    if (user) {
-      this.todo.setValue(user.name);
-    }
-
-    this.currentId.set(id);
-    this.isEditing.set(true);
-  }
-
-  protected handleSave() {
-    const name = this.todo.value?.trim();
-    if (!name) return alert('Name is required..!');
-    const idToEdit = this.currentId();
-
-    if (idToEdit && this.isEditing() !== null) {
-      this.users.update((currentUser: User[]) =>
-        currentUser.map((user: User) =>
-          user.id === idToEdit ? { ...user, name: name, email: `${name}@gmail.com` } : user
-        )
-      );
-      this.isEditing.set(false);
-      this.currentId.set(null);
-      this.todo.reset();
-      this.saveData();
-    } else {
-      this.create();
-    }
-  }
-
-  protected addTodo(): void {
-    this.create();
+    this.name.reset();
     this.saveData();
+  }
+  protected edit(id: number) {}
+  protected addUser() {
+    this.create();
   }
 }
