7d6f745be4d7b3ecbbfc54985bfbb5275cb13b1a
abdurashid-juraev <aj6243434@gmail.com>
2026-06-16 00:10:00 +0500

    fix

---
+++ src/app/features/profile/common/components/table/table.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  templateUrl: './table.html',
+  imports: [],
+  styles: ``,
+})
+export class MyTableComponent {}
